1edd418be874ce82dab3da9d6c68f51eb020fdac
Clouder0 <clouder0@outlook.com>
2024-07-12 20:40:39 +0800

    chore: change toolchain

--- xmake.lua
+++ xmake.lua
@@ -1,6 +1,28 @@
-add_requires("llvm 17.x", {alias = "llvm-17"})
-set_toolchains("llvm@llvm-17")
--- set_toolchains("clang")
+-- add_requires("llvm 17.x", {alias = "llvm-17"})
+--- set_toolchains("llvm@llvm-17")
+
+toolchain("my-clang")
+    -- mark as standalone toolchain
+    set_kind("standalone")
+
+    -- set toolset
+    set_toolset("cc", "clang-18")
+    set_toolset("cxx", "clang-18", "clang++-18")
+    set_toolset("sh", "clang++-18", "clang-18")
+    set_toolset("mm", "clang-18")
+    set_toolset("mxx", "clang-18", "clang++-18")
+    set_toolset("as", "clang-18")
+
+    add_defines("MYCLANG")
+
+    -- check toolchain
+    on_check(function (toolchain)
+        return import("lib.detect.find_tool")("clang")
+    end)
+
+toolchain_end()
+
+set_toolchains("my-clang")
 set_languages("c++23")
 set_options("debugger", "lldb")
 
